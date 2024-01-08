84ef40bc4becfc833b601dad1d8abb10f1f93fb1
kaos34k <yaristizabal34k@gmail.com>
2024-01-07 23:27:28 -0500

    Update model.go

--- accumulation/internal/domain/model.go
+++ accumulation/internal/domain/model.go
@@ -3,7 +3,6 @@ package domain
 type Point struct {
 	ID         string  `json:"id"`
 	User       string  `json:"user"`
-	Name       string  `json:"name"`
 	Total      float32 `json:"total"`
 	CreateDate string
 }
